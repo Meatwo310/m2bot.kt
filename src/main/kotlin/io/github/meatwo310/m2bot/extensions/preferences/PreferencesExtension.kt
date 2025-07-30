b73018cf6769e2f30abe603bdf314bea6338afaa
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-07-30 11:24:24 +0900

    feat(preferences): AI機能の管理を単純化し、関連する翻訳を更新

--- src/main/kotlin/io/github/meatwo310/m2bot/extensions/preferences/PreferencesExtension.kt
+++ src/main/kotlin/io/github/meatwo310/m2bot/extensions/preferences/PreferencesExtension.kt
@@ -2,15 +2,23 @@ package io.github.meatwo310.m2bot.extensions.preferences
 
 import dev.kord.rest.builder.message.allowedMentions
 import dev.kordex.core.commands.Arguments
-import dev.kordex.core.commands.application.slash.converters.impl.stringChoice
 import dev.kordex.core.commands.application.slash.publicSubCommand
 import dev.kordex.core.commands.converters.impl.optionalBoolean
 import dev.kordex.core.extensions.Extension
 import dev.kordex.core.extensions.publicSlashCommand
-import dev.kordex.core.i18n.toKey
+import dev.kordex.core.i18n.types.Key
 import dev.kordex.core.i18n.withContext
 import io.github.meatwo310.m2bot.i18n.Translations
-import kotlinx.serialization.descriptors.elementNames
+
+private fun Boolean.toKey() : Key {
+    return Translations.Commands.Preferences.let {
+        if (this) it.enabled else it.disabled
+    }
+}
+
+private fun Boolean.translate() : String {
+    return this.toKey().translate()
+}
 
 class PreferencesExtension : Extension() {
     override val name: String = "preferences"
@@ -23,51 +31,47 @@ class PreferencesExtension : Extension() {
             name = Translations.Commands.Preferences.name
             description = Translations.Commands.Preferences.description
 
-            publicSubCommand(::PreferencesBoolArgs) {
-                name = Translations.Commands.Preferences.Flag.name
-                description = Translations.Commands.Preferences.Flag.description
+            publicSubCommand(::BoolArgs) {
+                name = Translations.Commands.Preferences.Ai.name
+                description = Translations.Commands.Preferences.Ai.description
 
                 action {
-                    val key = arguments.key
                     val newValue = arguments.value
                     val currentPreferences = preferencesStorage.getOrDefault(user.id)
 
                     respond {
-                        if (newValue == null) {
-                            val currentValue = currentPreferences.getFieldValue<Boolean>(key)
-                            content = Translations.Commands.Preferences.print
-                                .withContext(this@action)
-                                .translateNamed("key" to key, "value" to currentValue.toString())
-                        } else {
-                            val updatedPreferences = currentPreferences.copyByFieldName(key, newValue)
-                            preferencesStorage.set(updatedPreferences)
-                            content = Translations.Commands.Preferences.update
-                                .withContext(this@action)
-                                .translateNamed("key" to key, "value" to newValue.toString())
-                        }
                         allowedMentions {}
+                        if (newValue == null) {
+                            content = Translations.Commands.Preferences.current
+                                .withContext(this@action)
+                                .translateNamed(
+                                    "feature" to Translations.Commands.Preferences.Ai.name,
+                                    "status" to currentPreferences.enableAI.toKey(),
+                                )
+                        } else {
+                            val updatedPreferences = currentPreferences
+                                .copy(enableAI = newValue)
+                                .also {
+                                    preferencesStorage.set(it)
+                                }
+                            preferencesStorage.set(updatedPreferences)
+                            content = Translations.Commands.Preferences.updated
+                                .withContext(this@action)
+                                .translateNamed(
+                                    "feature" to Translations.Commands.Preferences.Ai.name,
+                                    "status" to updatedPreferences.enableAI.toKey(),
+                                )
+                        }
                     }
                 }
             }
         }
     }
 
-    inner class PreferencesBoolArgs : Arguments() {
-        val key by stringChoice {
-            name = "key".toKey()
-            description = "The preference to manage".toKey()
-
-            PreferencesData.serializer().descriptor.elementNames
-                .filter { it != "userId" }
-                .filter { it.startsWith("enable") || it.startsWith("disable") }
-                .forEach {
-                    choice(it.toKey(), it)
-                }
-        }
-
+    inner class BoolArgs : Arguments() {
         val value by optionalBoolean {
-            name = "value".toKey()
-            description = "The value to set for the preference".toKey()
+            name = Translations.Commands.Preferences.Arguments.name
+            description = Translations.Commands.Preferences.Arguments.description
         }
     }
 }

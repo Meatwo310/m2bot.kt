56fc3f002a1a27697775852155c4aebb8e338cab
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-07-31 02:19:16 +0900

    feat(ai): Update AI assistant instructions and enhance response formatting

--- src/main/kotlin/io/github/meatwo310/m2bot/extensions/ai/AiExtension.kt
+++ src/main/kotlin/io/github/meatwo310/m2bot/extensions/ai/AiExtension.kt
@@ -5,6 +5,7 @@ import com.google.genai.types.*
 import dev.kord.core.behavior.channel.withTyping
 import dev.kord.core.behavior.reply
 import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.rest.builder.message.MessageBuilder
 import dev.kord.rest.builder.message.allowedMentions
 import dev.kordex.core.checks.isNotBot
 import dev.kordex.core.extensions.Extension
@@ -12,27 +13,35 @@ import dev.kordex.core.extensions.event
 import dev.kordex.core.utils.env
 import dev.kordex.core.utils.repliedMessageOrNull
 import io.github.meatwo310.m2bot.extensions.preferences.PreferencesExtension
+import kotlinx.serialization.json.Json
+import kotlin.jvm.optionals.getOrDefault
+import kotlin.jvm.optionals.getOrNull
 
 
 class AiExtension : Extension() {
-    override val name: String = "AiExtension"
+    override val name: String = "ai"
 
     companion object {
         private val googleApiKey = env("GOOGLE_API_KEY")
         val client = Client.builder()
             .apiKey(googleApiKey)
-            .build()
-        val instruction = Content.fromParts(Part.fromText(
-            "あなたは親切で、礼儀正しいAIアシスタントです。ユーザーの質問に答えてください。指示なき限り、LaTeXを使用せずに回答してください。"
-        ))!!
-        var googleSearchTool = Tool.builder()
-            .googleSearch(GoogleSearch.builder().build())
             .build()!!
-        var config = GenerateContentConfig.builder()
+        val instruction = Content.fromParts(Part.fromText(
+            "あなたは親切でフレンドリーなAIアシスタントです。ユーザーの質問へ簡潔に答えてください。指示なき限り、LaTeXを使用せずに、日本語で回答してください。"
+        ))!!
+        val googleSearchTool = Tool.builder()
+            .googleSearch(GoogleSearch.builder().build())
+            .urlContext(UrlContext.builder().build())
+            .codeExecution(ToolCodeExecution.builder().build())
+            .build()!!
+        val config = GenerateContentConfig.builder()
             .systemInstruction(instruction)
             .tools(listOf(googleSearchTool))
             .build()!!
 
+        val searchToolRegex = """print\(concise_search\((?:query=)?"(.*?)"(?:,.*)?\)\)""".toRegex()
+        val urlContextToolRegex = """print\(browse\((?:urls=)?(\[.*?\])\)\)""".toRegex()
+        val execResultRegex = """(?:Looking up information on Google Search\.|Browsing the web\.)\n?""".toRegex()
     }
 
     override suspend fun setup() {
@@ -57,9 +66,7 @@ class AiExtension : Extension() {
                         var message = event.message
                         repeat(10) {
                             message = message.repliedMessageOrNull() ?: return@apply
-                            val role = message.author?.let {
-                                if (it.isSelf) "model" else "user"
-                            } ?: return@apply
+                            val role = if (message.author?.isSelf ?: return@apply) "model" else "user"
                             add(Content.builder()
                                 .role(role)
                                 .parts(Part.fromText(message.content))
@@ -73,11 +80,57 @@ class AiExtension : Extension() {
                         config
                     ) ?: return@withTyping
                     event.message.reply {
-                        content = response.text() ?: "レスポンスの生成に失敗"
+                        content = buildString {
+                            response.parts()?.forEach {
+                                it.executableCode().getOrNull()?.code()?.getOrNull()?.let { code ->
+                                    when {
+                                        searchToolRegex.matches(code.trim()) -> {
+                                            appendLine(searchToolRegex.replace(code, """-# 🔎 Google検索: `$1`"""))
+                                            appendLine()
+                                        }
+                                        urlContextToolRegex.matches(code.trim()) -> {
+                                            appendLine("-# 🌐 Web閲覧:")
+                                            appendLine(urlContextToolRegex.replace(code) {
+                                                Json.decodeFromString<List<String>>(it.groupValues[1])
+                                                    .map { "-# - <$it>" }
+                                                    .joinToString("\n")
+                                            })
+                                            appendLine()
+                                        }
+                                        else -> {
+                                            appendLine("```python")
+                                            appendLine(code)
+                                        }
+                                    }
+                                }
+                                it.codeExecutionResult().getOrNull()?.output()?.getOrNull()?.let { output ->
+                                    if (execResultRegex.matches(output)) {
+                                        return@let
+                                    }
+
+                                    appendLine()
+                                    appendLine(output
+                                        .trimEnd()
+                                        .lines()
+                                        .joinToString("\n") {
+                                            line -> "# $line"
+                                        }
+                                    )
+                                    appendLine("```")
+                                }
+                                it.text().getOrNull()?.let { text ->
+                                    append(text)
+                                }
+                            }
+                        }.let {
+                            if (it.length > 2000)
+                                it.take(1997) + "..."
+                            else
+                                it
+                        }.ifBlank { "レスポンスの生成に失敗" }
                         allowedMentions {}
                     }
                 }
-
             }
         }
     }
